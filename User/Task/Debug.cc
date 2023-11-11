2c7c58470b53098c1574584d6732fc7aafc3652c
creeper5820 <cqq@1960180796@163.com>
2023-11-11 22:08:14 +0800

    放弃串口终端和变量管理器

--- User/Task/Debug.cc
+++ User/Task/Debug.cc
@@ -12,12 +12,8 @@ void DebugLoop();
 void DebugLoop()
 {
     SerialManager serial(&huart4);
-    ValueManager value;
-
-    value.New("VALUE", (float)12.5);
 
     while (1) {
-        // serial.HelloWorld(1);
-        osDelay(1000);
+        osDelay(100);
     }
 }
